732b5f05037b40d4362864bd1d35fb3775d5c177
Camilo Ramirez <camilo.ramirez@pragma.com.co>
2024-01-31 15:50:14 -0500

    - Creando modulos para gifs. Pasando valores al sidebar para mostrar las busquedas al dar clic

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,30 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+  <h5>Buscar:</h5>
+  <input type="text" class="form-control" placeholder="Buscar gifs"
+   (keyup.enter)="searchTag()"
+  #txtTagInput />
+  `,
+})
+export class SearchBoxComponent {
+
+  constructor(private _gifsService: GifsService) {
+
+  }
+
+
+  @ViewChild('txtTagInput')
+  tagInput!:ElementRef<HTMLInputElement>;
+
+
+  searchTag () {
+    const newTag = this.tagInput.nativeElement.value;
+    this._gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+
+}
